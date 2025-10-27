26ce45a131de1119e09dd70036f4c891f48b9aa2
suhaasiitb <suhaas.iitb@gmail.com>
2025-10-27 18:12:40 +0530

    ensure proper public path and nojekyll

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ev-service-portal/', // 👈 this line is critical for GitHub Pages
 })
