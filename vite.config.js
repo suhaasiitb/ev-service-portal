74438b75f06ff6092a1fd6686022ee40b7779ea8
suhaasiitb <suhaas.iitb@gmail.com>
2025-11-17 21:04:21 +0530

    Fix SPA routing for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    dedupe: ["react", "react-dom"]
-  },
-  base: "/ev-service-portal/",  
+  base: '/ev-service-portal/',
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'index.html',
+          dest: '.',
+          rename: '404.html'
+        }
+      ]
+    })
+  ]
 })
